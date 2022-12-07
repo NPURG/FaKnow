7abe15e571e013bf83c293ae2a823e5d240ddde8
YiyuanZhu <2643811975@qq.com>
2022-12-07 17:13:50 +0800

    update read event labels from txt

--- template/run/run_eann.py
+++ template/run/run_eann.py
@@ -1,5 +1,4 @@
 import os
-import pickle as pickle
 from typing import Dict, List, Any
 
 import numpy as np
@@ -14,24 +13,11 @@ from template.data.process.text_process import tokenize
 from train.eann_trainer import EANNTrainer
 
 
-def generate_post_event_map() -> Dict:
-    id_files = ['train', 'test', 'validate']
-    post_event_map = {}
-    for id_file in id_files:
-        id_ = pickle.load(
-            open(
-                "F:/code/python/EANN-KDD18-degugged11.2/Data/weibo/" + id_file + "_id.pickle",
-                'rb'))
-        post_event_map.update(id_)
-    return post_event_map
-
-
 # todo 将article_event_map中event id来代替event label
-def generate_event_label(post_id: str, post_event_map: Dict[str, int], event_label_map: Dict[int, int]) -> int:
-    event = int(post_event_map[post_id])
-    if event not in event_label_map:
-        event_label_map[event] = len(event_label_map)
-    event_label = event_label_map[event]
+def generate_event_label(event_id: int, event_label_map: Dict[int, int]) -> int:
+    if event_id not in event_label_map:
+        event_label_map[event_id] = len(event_label_map)
+    event_label = event_label_map[event_id]
     return event_label
 
 
@@ -45,7 +31,6 @@ def generate_mask_and_event_label(path: str, max_text_len: int):
     event_labels = []
     masks = []
     event_label_map = {}
-    post_event_map = generate_post_event_map()
 
     for dir in os.listdir(path):
         for entry in os.scandir(os.path.join(path, dir)):
@@ -56,17 +41,15 @@ def generate_mask_and_event_label(path: str, max_text_len: int):
                     for i, line in enumerate(f.readlines()):
                         line = line.rstrip()
 
-                        # 第1行 post_id
+                        # 第1行 event_id
                         if (i + 1) % 2 == 1:
-                            post_id = line
+                            event_id = int(line)
+                            event_labels.append(generate_event_label(event_id, event_label_map))
 
                         # 第2行 文本内容
                         if (i + 1) % 2 == 0:
                             tokens = tokenize(line)
-                            if post_id in post_event_map:
-                                event_labels.append(
-                                    generate_event_label(post_id, post_event_map, event_label_map))
-                                masks.append(generate_mask(len(tokens), max_text_len))
+                            masks.append(generate_mask(len(tokens), max_text_len))
     return torch.stack(masks), event_labels
 
 
@@ -117,10 +100,10 @@ def run_eann(root: str, pre_trained_word2vec=True, max_text_len: int = None, wor
         word_vectors, word_idx_map, max_text_len = build_word2vec(root)
         word_vectors, word_idx_map = padding_vec_and_idx(word_vectors, word_idx_map)
 
+    embedding_params = {'word_vectors': word_vectors, 'word_idx_map': word_idx_map, 'max_text_len': max_text_len}
+
     masks, event_labels = generate_mask_and_event_label(root, max_text_len)
     event_num = max(event_labels) + 1
-
-    embedding_params = {'word_vectors': word_vectors, 'word_idx_map': word_idx_map, 'max_text_len': max_text_len}
 
     dataset = FolderMultiModalDataset(root, embedding=eann_word2idx, transform=image_transforms,
                                       embedding_params=embedding_params,
@@ -145,38 +128,37 @@ def run_eann(root: str, pre_trained_word2vec=True, max_text_len: int = None, wor
 
 if __name__ == '__main__':
     root = "F:/code/python/EANN-KDD18-degugged11.2/test/pairdata"
-    text_path = 'F:/code/python/EANN-KDD18-degugged11.2/test/pairdata/fake/3464577842643036.txt'
+    run_eann(root, False)
+    # image_transforms = transforms.Compose([
+    #     transforms.Resize(256),
+    #     transforms.CenterCrop(224),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
+    #
+    # word_vectors, word_idx_map, max_text_len = build_word2vec(root, vector_size=32)
+    # word_vectors, word_idx_map = padding_vec_and_idx(word_vectors, word_idx_map)
+    # masks, event_labels = generate_mask_and_event_label(root, max_text_len)
+    # event_num = max(event_labels) + 1
 
-    image_transforms = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    word_vectors, word_idx_map, max_text_len = build_word2vec(root, vector_size=32)
-    word_vectors, word_idx_map = padding_vec_and_idx(word_vectors, word_idx_map)
-    masks, event_labels = generate_mask_and_event_label(root, max_text_len)
-    event_num = max(event_labels) + 1
-
-    embedding_params = {'word_vectors': word_vectors, 'word_idx_map': word_idx_map, 'max_text_len': max_text_len}
-
-    dataset = FolderMultiModalDataset(root, embedding=eann_word2idx, transform=image_transforms,
-                                      embedding_params=embedding_params,
-                                      mask=masks, event_label=torch.tensor(event_labels))
-
-    model = EANN(event_num,
-                 hidden_size=32,
-                 embed_dim=word_vectors.shape[0],
-                 dropout=1,
-                 reverse_lambd=1,
-                 vocab_size=len(word_idx_map),
-                 embed_weight=word_vectors)
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad,
-                                        list(model.parameters())),
-                                 lr=0.0003)
-    evaluator = Evaluator(['accuracy', 'precision', 'recall', 'f1'])
-
-    trainer = EANNTrainer(model, evaluator, criterion, optimizer)
-    trainer.fit(dataset, batch_size=20, epochs=100, validate_size=0.2, saved=True)
+    # embedding_params = {'word_vectors': word_vectors, 'word_idx_map': word_idx_map, 'max_text_len': max_text_len}
+    #
+    # dataset = FolderMultiModalDataset(root, embedding=eann_word2idx, transform=image_transforms,
+    #                                   embedding_params=embedding_params,
+    #                                   mask=masks, event_label=torch.tensor(event_labels))
+    #
+    # model = EANN(event_num,
+    #              hidden_size=32,
+    #              embed_dim=word_vectors.shape[0],
+    #              dropout=1,
+    #              reverse_lambd=1,
+    #              vocab_size=len(word_idx_map),
+    #              embed_weight=word_vectors)
+    # criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad,
+    #                                     list(model.parameters())),
+    #                              lr=0.0003)
+    # evaluator = Evaluator(['accuracy', 'precision', 'recall', 'f1'])
+    #
+    # trainer = EANNTrainer(model, evaluator, criterion, optimizer)
+    # trainer.fit(dataset, batch_size=20, epochs=100, validate_size=0.2, saved=True)
