09964e360a3b8d10642c655d46855e302b868693
YiyuanZhu <2643811975@qq.com>
2023-02-12 23:23:50 +0800

    update: default Evaluator with all metrics

--- template/evaluate/evaluator.py
+++ template/evaluate/evaluator.py
@@ -6,18 +6,21 @@ from template.evaluate.metrics import get_metric_func
 
 
 class Evaluator:
-    def __init__(self, metrics: List[Union[str, Callable]]):
+    def __init__(self, metrics: List[Union[str, Callable]]=None):
         """generate metric functions for evaluator
         Args:
             metrics: a list of metrics, str or Callable.
                      If the metric is str, built-in metric functions(`accuracy`, `precision`, `recall`, `f1`) will be called according to the metric name.
                      Or the passing metric function would be called
         """
+        if metrics is None:
+            metrics = ['accuracy', 'precision', 'recall', 'f1']
         self.metrics = {}
+
         for metric in metrics:
             if type(metric) == str:
                 self.metrics[metric] = get_metric_func(metric)
-            elif type(metric) == Callable:
+            elif isinstance(metric, Callable):
                 self.metrics[metric.__name__] = metric
             else:
                 raise RuntimeError(f'only str or callable are supported as metrics, but {type(metric)} are provided')
