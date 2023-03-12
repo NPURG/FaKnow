5561d432e25d997481a12c5a7122b9083d276f73
YiyuanZhu <2643811975@qq.com>
2023-03-12 21:00:06 +0800

    update: add docs

--- template/model/textcnn.py
+++ template/model/textcnn.py
@@ -2,7 +2,7 @@ from typing import Callable, Optional, List
 
 import torch
 import torch.nn.functional as F
-from torch import nn
+from torch import nn, Tensor
 
 from model.layers.layer import TextCNNLayer
 from model.model import AbstractModel
@@ -15,6 +15,17 @@ code: https://github.com/yoonkim/CNN_sentence
 
 
 class TextCNN(AbstractModel):
+    r"""TextCNN model with a TextCNN layer and a fully connected layer
+
+        Args:
+            word_vectors (torch.Tensor): weights of word embedding layer, shape=(vocab_size, embedding_size)
+            filter_num (int): number of filters in conv layer
+            kernel_sizes (List[int]): list of different kernel sizes for TextCNNLayer
+            activate_func (Callable): activate function for TextCNNLayer. Default=relu
+            dropout (float): drop out rate of fully connected layer. Default=0.5
+            freeze (bool): whether to freeze weights in word embedding layer while training. Default=True
+        """
+
     def __init__(self,
                  word_vectors: torch.Tensor,
                  filter_num: int,
@@ -34,7 +45,7 @@ class TextCNN(AbstractModel):
             nn.Linear(len(kernel_sizes) * filter_num, 2)
         )
 
-    def forward(self, text: torch.Tensor):
+    def forward(self, text: torch.Tensor) -> Tensor:
         text = self.word_embedding(text)
         text = self.text_ccn_layer(text)
         out = self.classifier(text)
