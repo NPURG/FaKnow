2bb6055fb93eee5fc5a109f18e92efa55078b640
jialiangwww <jialiangwang@163.com>
2023-05-04 17:52:27 +0800

    add run way

--- faknow/run/run_safe.py
+++ faknow/run/run_safe.py
@@ -10,31 +10,31 @@ from faknow.train.trainer import BaseTrainer
 from faknow.utils.util import dict2str
 
 
-def run_safe(root: str):
-    dataset = SAFENumpyDataset(root)
+def run_safe(root):
+        dataset = SAFENumpyDataset(root)
 
-    val_size = int(len(dataset) * 0.1)
-    test_size = int(len(dataset) * 0.2)
-    train_size = len(dataset) - val_size - test_size
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+        val_size = int(len(dataset) * 0.1)
+        test_size = int(len(dataset) * 0.2)
+        train_size = len(dataset) - val_size - test_size
+        train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-    model = SAFE()
-    optimizer = torch.optim.Adam(
-        filter(
-            lambda p: p.requires_grad,
-            list(model.parameters())),
-        lr=0.00025
-    )
-    evaluator = Evaluator(["accuracy", "precision", "recall", "f1"])
+        model = SAFE()
+        optimizer = torch.optim.Adam(
+            filter(
+                lambda p: p.requires_grad,
+                list(model.parameters())),
+            lr=0.00025
+        )
+        evaluator = Evaluator(["accuracy", "precision", "recall", "f1"])
 
-    trainer = BaseTrainer(model, evaluator, optimizer)
-    trainer.fit(train_loader=train_loader, num_epoch=100, validate_loader=val_loader, save=True)
-    test_result = trainer.evaluate(test_loader)
-    print("test result: ", {dict2str(test_result)})
+        trainer = BaseTrainer(model, evaluator, optimizer)
+        trainer.fit(train_loader=train_loader, num_epoch=100, validate_loader=val_loader, save=True)
+        test_result = trainer.evaluate(test_loader)
+        print("test result: ", {dict2str(test_result)})
 
 
 def main():
---
+++ run_safe.py
@@ -0,0 +1,4 @@
+from faknow.run.run_safe import main
+
+if __name__ == '__main__':
+    main()
