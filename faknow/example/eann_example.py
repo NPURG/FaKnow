1644d5e0d3ca5087966c68a4ff4278169689a398
YiyuanZhu <2643811975@qq.com>
2023-06-19 16:00:58 +0800

    delete: remove example folder

--- faknow/example/eann_example.py
+++
@@ -1,5 +0,0 @@
-from faknow.run.content_based.multimodal.run_eann import run_eann
-
-if __name__ == '__main__':
-    root = 'F:/code/python/EANN-KDD18-degugged11.2/test/pairdata'
-    run_eann(root, False)
