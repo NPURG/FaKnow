eb4acd3b6b6dfec8e512a24f7deb1698a13c12e4
YiyuanZhu <2643811975@qq.com>
2023-07-20 21:26:57 +0800

    update: annotation for TextCNN

--- faknow/run/content_based/run_textcnn.py
+++ faknow/run/content_based/run_textcnn.py
@@ -18,7 +18,23 @@ __all__ = ['TokenizerTextCNN', 'run_textcnn', 'run_textcnn_from_yaml']
 
 
 class TokenizerTextCNN:
-    def __init__(self, vocab: Dict[str, int], max_len=255, stop_words: List[str] = None, language='zh') -> None:
+    """
+    tokenizer for TextCNN
+    """
+
+    def __init__(self,
+                 vocab: Dict[str, int],
+                 max_len=255,
+                 stop_words: List[str] = None,
+                 language='zh') -> None:
+        """
+        Args:
+            vocab (Dict[str, int]): vocabulary of the corpus
+            max_len (int): max length of the text, default=255
+            stop_words (List[str]): stop words, default=None
+            language (str): language of the corpus, 'zh' or 'en', default='zh'
+        """
+
         assert language in ['zh', 'en'], "language must be one of {zh, en}"
         self.language = language
         self.vocab = vocab
@@ -28,15 +44,28 @@ class TokenizerTextCNN:
         self.stop_words = stop_words
 
     def __call__(self, texts: List[str]) -> torch.Tensor:
+        """
+        tokenize texts
+
+        Args:
+            texts (List[str]): texts to be tokenized
+
+        Returns:
+            torch.Tensor: tokenized texts
+        """
+
         token_ids = []
         for text in texts:
-            cleaned_text = re.sub(u"[，。 :,.；|-“”——_/nbsp+&;@、《》～（）())#O！：【】]", "", text).strip().lower()
+            cleaned_text = re.sub(u"[，。 :,.；|-“”——_/nbsp+&;@、《》～（）())#O！：【】]",
+                                  "", text).strip().lower()
 
             if self.language == 'zh':
                 split_words = jieba.cut(cleaned_text)
             else:
                 split_words = cleaned_text.split()
-            words = [word for word in split_words if word not in self.stop_words]
+            words = [
+                word for word in split_words if word not in self.stop_words
+            ]
 
             token_id = [self.vocab[word] for word in words]
             real_len = len(token_id)
@@ -67,6 +96,31 @@ def run_textcnn(train_path: str,
                 validate_path: str = None,
                 test_path: str = None,
                 device='cpu') -> None:
+    """
+    run TextCNN, including training, validation and testing.
+    If validate_path and test_path are None, only training is performed.
+
+    Args:
+        train_path (str): path of the training set
+        vocab (Dict[str, int]): vocabulary of the corpus
+        stop_words (List[str]): stop words
+        word_vectors (torch.Tensor): word vectors
+        language (str): language of the corpus, 'zh' or 'en', default='zh'
+        max_len (int): max length of the text, default=255
+        filter_num (int): number of filters, default=100,
+        kernel_sizes (List[int]): list of different kernel_num sizes for TextCNNLayer, if None, [3, 4, 5] is taken as default, default=None
+        activate_func (Callable): activate function for TextCNNLayer, default=relu
+        dropout (float): drop out rate of fully connected layer, default=0.5
+        freeze (bool): whether to freeze weights in word embedding layer while training,default=False
+        batch_size (int): batch size, default=100
+        lr (float): learning rate, default=0.001
+        num_epochs (int): number of epochs, default=100
+        metrics (List): metrics, if None, ['accuracy', 'precision', 'recall', 'f1'] is used, default=None
+        validate_path (str): path of the validation set, default=None
+        test_path (str): path of the test set, default=None
+        device (str): device, default='cpu'
+    """
+
     tokenizer = TokenizerTextCNN(vocab, max_len, stop_words, language)
 
     train_set = TextDataset(train_path, ['text'], tokenizer)
@@ -78,10 +132,10 @@ def run_textcnn(train_path: str,
     else:
         val_loader = None
 
-    model = TextCNN(word_vectors, filter_num, kernel_sizes, activate_func, dropout, freeze)
-    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad,
-                                        list(model.parameters())),
-                                 lr)
+    model = TextCNN(word_vectors, filter_num, kernel_sizes, activate_func,
+                    dropout, freeze)
+    optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, list(model.parameters())), lr)
     evaluator = Evaluator(metrics)
     trainer = BaseTrainer(model, evaluator, optimizer, device=device)
     trainer.fit(train_loader, num_epochs, validate_loader=val_loader)
@@ -94,6 +148,16 @@ def run_textcnn(train_path: str,
 
 
 def _parse_kargs(config: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    parse kargs from config dict
+
+    Args:
+        config (Dict[str, Any]): config dict, keys are the same as the args of `run_textcnn`
+
+    Returns:
+        Dict[str, Any]: converted kargs
+    """
+
     with open(config['vocab'], 'rb') as f:
         config['vocab'] = pickle.load(f)
     with open(config['word_vectors'], 'rb') as f:
@@ -103,6 +167,13 @@ def _parse_kargs(config: Dict[str, Any]) -> Dict[str, Any]:
 
 
 def run_textcnn_from_yaml(path: str):
+    """
+    run TextCNN from yaml config file
+
+    Args:
+        path (str): yaml config file path
+    """
+
     with open(path, 'r', encoding='utf-8') as _f:
         _config = yaml.load(_f, Loader=yaml.FullLoader)
         run_textcnn(**_parse_kargs(_config))
