54a22d2270ffce5f0983bf35092b5792990d93ff
YiyuanZhu <2643811975@qq.com>
2023-07-22 17:19:49 +0800

    update: annotation for AbstractModel

--- faknow/model/model.py
+++ faknow/model/model.py
@@ -5,15 +5,37 @@ from torch import Tensor
 
 
 class AbstractModel(nn.Module):
-    """abstract class for all models"""
+    """
+    abstract class for all models, every model should inherit it and implement the following methods:
+    1. calculate_loss
+    2. predict
+    """
 
     def __init__(self):
         super(AbstractModel, self).__init__()
 
     def calculate_loss(self, data) -> Union[Tensor, Dict[str, Tensor]]:
+        """
+        calculate loss
+
+        Args:
+            data: batch data
+
+        Returns:
+            Union[Tensor, Dict[str, Tensor]]: loss or a dict of loss if there are multiple losses
+        """
         raise NotImplementedError
 
     def predict(self, data_without_label) -> Tensor:
+        """
+        predict the probability of being fake news
+
+        Args:
+            data_without_label: batch data
+
+        Returns:
+            Tensor: probability, shape=(batch_size, 2)
+        """
         raise NotImplementedError
 
     # todo 如果用val loss进行early stopping
