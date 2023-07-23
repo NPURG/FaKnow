fe5458cbb23184a4b421194ec4cbbe01a452da63
YiyuanZhu <2643811975@qq.com>
2023-07-23 19:58:50 +0800

    update: annotation for UPFD

--- faknow/run/social_context/run_upfd.py
+++ faknow/run/social_context/run_upfd.py
@@ -25,6 +25,36 @@ def run_upfd(root: str,
              weight_decay=0.01,
              metrics: List = None,
              device='cpu'):
+    r"""
+    run UPFD using UPFD dataset, including training, validation and testing.
+    If validation and testing data are not provided, only training is performed.
+
+    Args:
+        root (str): Root directory where the dataset should be saved
+        name (str): The name of the graph set (:obj:`"politifact"`, :obj:`"gossipcop"`)
+        feature (str): The node feature type (:obj:`"profile"`, :obj:`"spacy"`, :obj:`"bert"`, :obj:`"content"`)
+            If set to :obj:`"profile"`, the 10-dimensional node feature
+            is composed of ten Twitter user profile attributes.
+            If set to :obj:`"spacy"`, the 300-dimensional node feature is
+            composed of Twitter user historical tweets encoded by
+            the `spaCy word2vec encoder
+            <https://spacy.io/models/en#en_core_web_lg>`_.
+            If set to :obj:`"bert"`, the 768-dimensional node feature is
+            composed of Twitter user historical tweets encoded by the
+            `bert-as-service <https://github.com/hanxiao/bert-as-service>`_.
+            If set to :obj:`"content"`, the 310-dimensional node feature is
+            composed of a 300-dimensional "spacy" vector plus a
+            10-dimensional "profile" vector.
+        splits (List[str]): dataset split, including 'train', 'val' and 'test'.
+            If None, ['train', 'val', 'test'] will be used, default=None
+        base_model (str): base model for UPFD, including 'sage', 'gcn', 'gat', 'gcnfn', default='sage'
+        batch_size (int): batch size, default=128
+        epochs (int): number of epochs, default=75
+        lr (float): learning rate, default=0.01
+        weight_decay (float): weight decay, default=0.01
+        metrics (List): evaluation metrics, if None, ['accuracy', 'precision', 'recall', 'f1'] is used, default=None
+        device (str): device, default='cpu'
+    """
 
     assert base_model in [
         'sage', 'gcn', 'gat', 'gcnfn'
