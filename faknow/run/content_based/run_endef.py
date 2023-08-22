bcee548031189168ba019b2e73809a36b0250c98
mingghao <1531286264@qq.com>
2023-08-22 18:34:38 +0800

    add run_endef

---
+++ faknow/run/content_based/run_endef.py
@@ -0,0 +1,114 @@
+from typing import List, Dict
+
+import torch
+import yaml
+from torch.utils.data import DataLoader
+from transformers import BertTokenizer
+
+from faknow.data.dataset.text import TextDataset
+from faknow.evaluate.evaluator import Evaluator
+from faknow.model.content_based.endef import ENDEF
+from faknow.model.content_based.mdfend import MDFEND
+from faknow.train.trainer import BaseTrainer
+from faknow.utils.util import dict2str
+
+__all__ = ['TokenizerENDEF', 'run_endef', 'run_endef_from_yaml']
+
+class TokenizerENDEF:
+    """Tokenizer for ENDEF"""
+    def __init__(self, max_len=170, bert="hfl/chinese-roberta-wwm-ext"):
+        """
+
+        """
+        self.max_len = max_len
+        self.tokenizer = BertTokenizer.from_pretrained(bert)
+
+    def __call__(self, texts: List[str]) -> Dict[str, torch.Tensor]:
+        """
+
+        tokenize texts
+
+        Args:
+            texts(List[str]): texts to be tokenized
+
+        Returns:
+            Dict[str, torch.Tensor]: tokenized texts with key 'token_id' and 'mask'
+        """
+
+        token_id = []
+        attention_mask = []
+        for _, text in enumerate(texts):
+            inputs = self.tokenizer(text,
+                                    return_tensors='pt',
+                                    max_length=self.max_len,
+                                    add_special_tokens=True,
+                                    padding='max_length',
+                                    truncation=True)
+            token_id.append(inputs['input_ids'])
+            attention_mask.append(inputs['attention_mask'])
+
+        token_id = torch.cat(token_id, dim=0)
+        attention_mask = torch.cat(attention_mask, dim=0)
+
+        return{
+            'token_id': token_id,
+            'mask': attention_mask
+        }
+
+def run_endef(train_path: str,
+               bert='hfl/chinese-roberta-wwm-ext',
+               max_len=170,
+               domain_num=8,
+               batch_size=64,
+               num_epochs=50,
+               lr=0.0005,
+               weight_decay=5e-5,
+               step_size=100,
+               gamma=0.98,
+               metrics: List = None,
+               validate_path: str = None,
+               test_path: str = None,
+               device='cpu'):
+
+    tokenizer = TokenizerENDEF(max_len, bert)
+    train_set = TextDataset(train_path, ['text', 'entity'], tokenizer)
+    train_loader = DataLoader(train_set, batch_size, shuffle=True)
+
+    if validate_path is not None:
+        validate_set = TextDataset(validate_path, ['text', 'entity'], tokenizer)
+        val_loader = DataLoader(validate_set, batch_size, shuffle=False)
+    else:
+        val_loader = None
+
+    model = ENDEF(bert,base_model=MDFEND(bert, domain_num))
+    optimizer = torch.optim.Adam(params=model.parameters(),
+                                 lr=lr,
+                                 weight_decay=weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size, gamma)
+    evaluator = Evaluator(metrics)
+
+    trainer = BaseTrainer(model,
+                          evaluator,
+                          optimizer,
+                          scheduler,
+                          device=device)
+    trainer.fit(train_loader, num_epochs, validate_loader=val_loader)
+
+    if test_path is None:
+        test_set = TextDataset(test_path, ['text', 'entity'], tokenizer)
+        test_loader = DataLoader(test_set, batch_size, shuffle=False)
+        test_result = trainer.evaluate(test_loader)
+        print('test result: ', dict2str(test_result))
+
+def run_endef_from_yaml(path: str):
+    """
+    run MDFENDENDEF from yaml config file
+
+    Args:
+        path(str): yaml config file path
+
+    """
+
+    with open(path, 'r', encoding='utf-8') as _f:
+        _config = yaml.load(_f, Loader=yaml.FullLoader)
+        run_mdfendendef(_config)
