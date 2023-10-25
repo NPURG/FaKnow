62a47bcb766c2970928b4e192699a94b6cc2abef
YiyuanZhu <2643811975@qq.com>
2023-10-25 19:57:02 +0800

    fix: metrics args with sklearn

--- faknow/evaluate/metrics.py
+++ faknow/evaluate/metrics.py
@@ -1,5 +1,6 @@
 import torch
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import (precision_score, recall_score,
+                             f1_score, roc_auc_score)
 from typing import Callable
 
 
@@ -24,10 +25,12 @@ def get_metric_func(name: str) -> Callable:
         return calculate_recall
     if name == 'f1':
         return calculate_f1
+    if name == 'auc':
+        return calculate_auc
     raise RuntimeError(f'no metric function called {name}')
 
 
-def calculate_accuracy(outputs: torch.Tensor, y: torch.Tensor):
+def calculate_accuracy(outputs: torch.Tensor, y: torch.Tensor) -> float:
     """Calculate the accuracy metric.
 
     Args:
@@ -40,7 +43,7 @@ def calculate_accuracy(outputs: torch.Tensor, y: torch.Tensor):
     return (outputs.argmax(dim=1).detach().cpu() == y.cpu()).float().mean().item()
 
 
-def calculate_precision(outputs: torch.Tensor, y: torch.Tensor):
+def calculate_precision(outputs: torch.Tensor, y: torch.Tensor) -> float:
     """Calculate the precision metric.
 
     Args:
@@ -50,10 +53,12 @@ def calculate_precision(outputs: torch.Tensor, y: torch.Tensor):
     Returns:
         float: The precision value.
     """
-    return precision_score(outputs.argmax(dim=1).detach().cpu().numpy(), y.cpu().numpy(), zero_division=0)
+    return precision_score(y.cpu().numpy(),
+                           outputs.argmax(dim=1).detach().cpu().numpy(),
+                           zero_division=0)
 
 
-def calculate_recall(outputs: torch.Tensor, y: torch.Tensor):
+def calculate_recall(outputs: torch.Tensor, y: torch.Tensor) -> float:
     """Calculate the recall metric.
 
     Args:
@@ -63,10 +68,12 @@ def calculate_recall(outputs: torch.Tensor, y: torch.Tensor):
     Returns:
         float: The recall value.
     """
-    return recall_score(outputs.argmax(dim=1).detach().cpu().numpy(), y.cpu().numpy(), zero_division=0)
+    return recall_score(y.cpu().numpy(),
+                        outputs.argmax(dim=1).detach().cpu().numpy(),
+                        zero_division=0)
 
 
-def calculate_f1(outputs: torch.Tensor, y: torch.Tensor):
+def calculate_f1(outputs: torch.Tensor, y: torch.Tensor) -> float:
     """Calculate the F1 score metric.
 
     Args:
@@ -76,4 +83,21 @@ def calculate_f1(outputs: torch.Tensor, y: torch.Tensor):
     Returns:
         float: The F1 score value.
     """
-    return f1_score(outputs.argmax(dim=1).detach().cpu().numpy(), y.cpu().numpy(), zero_division=0)
+    return f1_score(y.cpu().numpy(),
+                    outputs.argmax(dim=1).detach().cpu().numpy(),
+                    zero_division=0)
+
+
+def calculate_auc(outputs: torch.Tensor, y: torch.Tensor) -> float:
+    """Calculate the AUC score metric.
+
+    Args:
+        outputs (torch.Tensor): Model's predictions.
+        y (torch.Tensor): Ground truth labels.
+
+    Returns:
+        float: The AUC score value.
+    """
+
+    return roc_auc_score(y.cpu().numpy(),
+                         outputs.argmax(dim=1).detach().cpu().numpy())
