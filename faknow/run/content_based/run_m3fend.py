db0b68e44ca9e5c19a54e2b193f9b96503818a19
jialiangwww <jia_liang_wang@163.com>
2023-11-24 20:24:54 +0800

    fix: cpu or cuda

--- faknow/run/content_based/run_m3fend.py
+++ faknow/run/content_based/run_m3fend.py
@@ -15,7 +15,7 @@ from faknow.model.content_based.m3fend import M3FEND
 from faknow.train.m3fend_trainer import M3FENDTrainer
 from faknow.train.trainer import BaseTrainer
 from faknow.utils.util import EarlyStopping
-from faknow.utils.utils_m3fend import Recorder, data2gpu, Averager, metrics
+from faknow.utils.utils_m3fend import Recorder, data2gpu, Averager, metrics, tuple2dict
 
 seed = 2021
 random.seed(seed)
@@ -47,7 +47,6 @@ def run_m3fend(
         domain_num: int = 3,
         emb_dim: int = 768,
         mlp_dims: list = [384],
-        use_cuda: bool = True,
         batch_size: int = 64,
         num_workers: int = 4,
         max_len: int = 170,
@@ -63,9 +62,10 @@ def run_m3fend(
         early_stop: int = 3,
         epochs: int = 50,
         device: str = 'cpu',
-        gpu: int = 0,
+        gpu: str = '0',
         metrics: List = None,
 ):
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu  # 将 CUDA_VISIBLE_DEVICES 环境变量设置为在命令行参数中指定的 GPU 的值
     if dataset == 'en':
         root_path = '../../../dataset/example/M3FEND/en/'
         category_dict = {
@@ -150,6 +150,8 @@ def run_m3fend(
 
     # model
     model = M3FEND(emb_dim, mlp_dims, dropout, semantic_num, emotion_num, style_num, lnn_dim, len(category_dict), dataset)
+    if device == 'cuda':
+        model = model.cuda()
 
     # loss
     # 在模型的 calculate_loss 函数中体现
@@ -164,17 +166,15 @@ def run_m3fend(
     evaluator = Evaluator(metrics)
 
     # todo early_stopping
-    early_stopping = EarlyStopping(patience=3)
+    early_stopping = EarlyStopping(patience=early_stop)
 
     # 设置模型为训练模式
     model.train()
 
-    # 创建进度条对象
-    train_data_iter = tqdm(train_loader)
-
     # 循环遍历训练数据 : 将数据移动到GPU + 将所有样本的归一化特征按照它们的领域（domain）信息保存在self.all_feature 字典中
+    train_data_iter = tqdm(train_loader, desc='Moving data to device')
     for step_n, batch in enumerate(train_data_iter):
-        batch_data = data2gpu(batch, use_cuda)
+        batch_data = data2gpu(batch, device)
         label_pred = model.save_feature(**batch_data)
 
     # Domain Event Memory 初始化
@@ -189,54 +189,9 @@ def run_m3fend(
         early_stopping=early_stopping,
         device=device
     )
-    trainer.fit(train_loader=train_loader, num_epochs=epochs, validate_loader=val_loader)
-
-
-"""    trainer = M3FENDTrainer(emb_dim=emb_dim, mlp_dims=mlp_dims, use_cuda=use_cuda, lr=lr,
-                            train_loader=train_loader, dropout=dropout, weight_decay=weight_decay,
-                            val_loader=val_loader, test_loader=test_loader, category_dict=category_dict,
-                            early_stop=early_stop, epochs=epochs,
-                            save_param_dir=os.path.join(save_param_dir, 'm3fend'),
-                            semantic_num=semantic_num, emotion_num=emotion_num, style_num=style_num,
-                            lnn_dim=lnn_dim, dataset=dataset)
-
-    # 简单的参数搜索过程，对于给定的参数进行多次训练，记录并比较它们的性能，最终输出最佳的参数配置和相应的模型
-    train_param = {
-        'lr': [lr] * 10
-    }
-    print(train_param)
-
-    param = train_param
-    best_param = []  # 用于存储每个参数的最佳取值
-
-    json_path = './logs/json/' + 'm3fend.json'
-    json_result = []
-    for p, vs in param.items():  # param是一个字典，键：参数名称，值：参数可能取值的列表
-        best_metric = {}
-        best_metric['metric'] = 0
-        best_v = vs[0]
-        best_model_path = None
-        for i, v in enumerate(vs):
-            setattr(trainer, p, v)
-            metrics, model_path = trainer.train(logger)
-            json_result.append(metrics)
-            if (metrics['metric'] > best_metric['metric']):
-                best_metric = metrics
-                best_v = v
-                best_model_path = model_path
-        best_param.append({p: best_v})
-
-        print("best model path:", best_model_path)
-        print("best metric:", best_metric)
-        logger.info("best model path:" + best_model_path)
-        logger.info("best param " + p + ": " + str(best_v))
-        logger.info("best metric:" + str(best_metric))
-        logger.info('--------------------------------------\n')
-    with open(json_path, 'w') as file:
-        json.dump(json_result, file, indent=4, ensure_ascii=False)"""
-
-
+    trainer.fit(train_loader=train_loader, num_epochs=epochs)
+    # trainer.fit(train_loader=train_loader, num_epochs=epochs, validate_loader=val_loader)
 
 
 if __name__ == '__main__':
-    run_m3fend(dataset='en', domain_num=3, lr=0.0001, gpu=0)
+    run_m3fend(dataset='en', domain_num=3, lr=0.0001, device='cuda', gpu='0')
