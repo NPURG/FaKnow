8174dfd2e05b44938dd3c97bbde981af2d45cc3b
jialiangwww <jia_liang_wang@163.com>
2024-01-31 20:46:40 +0800

    add: import  recommonmark

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -8,6 +8,7 @@
 
 import os
 import sys
+import recommonmark
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../faknow/model/content_based/'))
 sys.path.insert(0, os.path.abspath('../../faknow/run/content_based/'))
